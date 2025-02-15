a94012cfd1eafee31b1ec7caf23b5214d5fe73c3
Prarthana Bhalerao <bhaleraoprarthana03@gmail.com>
2025-02-15 10:57:06 +0530

    modify

--- src/App.js
+++ src/App.js
@@ -1,30 +1,28 @@
 import "./App.css";
+import gptlogo from "../src/assets/chatgpt.svg";
 
 function App() {
   return (
     <div className="App">
-      <div className="sidebar">
+      <div className="sideBar">
         <div className="upperSide">
-          <img src="" alt="" className="logo" />
-          <span className="brand">ChatGPT</span>
-          <button className="midbtn">
-            <img src="" alt="" className="addbtn" />
-            New Chat
-          </button>
-          <div className="sidebottom">
-            <button className="query">
-              <img src="" alt="" className="" />
-              What is programming?
-            </button>
-            <button className="query">
-              <img src="" alt="" className="" />
-              What is Web Development?
-            </button>
+          <div className="upperSideTop">
+            <img src={gptlogo} alt="logo" className="logo" />
+            <span className="brand">ChatGPT</span>
+            <button className="midbtn">New Chat</button>
+          </div>
+          <div className="upperSideBottom">
+            <button className="query">What is programming?</button>
+            <button className="query">What is Web Development?</button>
           </div>
         </div>
-        <div className="lowerSide"></div>
+        <div className="lowerSide">
+          <p>Lower Sidebar Content</p>
+        </div>
       </div>
-      <div className="main"></div>
+      <div className="main">
+        <h1>Main Content</h1>
+      </div>
     </div>
   );
 }
