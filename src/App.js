a35146459a91af0ab61e0821251df9dea6833af1
Prarthana Bhalerao <bhaleraoprarthana03@gmail.com>
2025-02-15 18:31:23 +0530

    modify

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import "./App.css";
 import gptlogo from "../src/assets/chatgpt.svg";
+import addBtn from "../src/assets/add-30.png";
+import msgIcon from "../src/assets/message.svg";
+import home from "../src/assets/home.svg";
+import saved from "../src/assets/bookmark.svg";
+import rocket from "../src/assets/rocket.svg";
 
 function App() {
   return (
@@ -9,15 +14,31 @@ function App() {
           <div className="upperSideTop">
             <img src={gptlogo} alt="logo" className="logo" />
             <span className="brand">ChatGPT</span>
-            <button className="midbtn">New Chat</button>
+            <button className="midbtn">
+              <img src={addBtn} alt="Newchat" className="addBtn" />
+              New Chat
+            </button>
           </div>
           <div className="upperSideBottom">
-            <button className="query">What is programming?</button>
-            <button className="query">What is Web Development?</button>
+            <button className="query">
+              <img src={msgIcon} alt="query" /> What is programming?
+            </button>
+            <button className="query">
+              <img src={msgIcon} alt="query" />
+              What is Web Development?
+            </button>
           </div>
         </div>
+
         <div className="lowerSide">
-          <p>Lower Sidebar Content</p>
+          <div className="listItems">
+            <img src={home} alt="" className="listItemImg" />
+            Home
+            <img src={saved} alt="" className="listItemImg" />
+            Saved
+            <img src={rocket} alt="" className="listItemImg" />
+            Upgrade to Pro
+          </div>
         </div>
       </div>
       <div className="main">
