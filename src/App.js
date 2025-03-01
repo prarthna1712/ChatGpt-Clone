48ef5dd4d0034731aa8f566b10eb519f0e7acc86
Prarthana Bhalerao <bhaleraoprarthana03@gmail.com>
2025-03-01 10:52:39 +0530

    app js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,14 @@ import rocket from "../src/assets/rocket.svg";
 import sendBtn from "../src/assets/send.svg";
 import userIcon from "../src/assets/passportsizeprofilepic.jpeg";
 import gptImgLogo from "../src/assets/chatgptLogo.svg";
+import { sendMsgToOpenAI } from "./openai";
+import { useState } from "react";
 function App() {
+  const [input, setInput] = useState("");
+  const handleSend = () => {
+    const res = sendMsgToOpenAI();
+  };
+
   return (
     <div className="App">
       <div className="sideBar">
